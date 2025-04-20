f9124e9125e8680406a6c9027cbda1176aeadf97
TheGitHubNoob112 <131225671+TheGitHubNoob112@users.noreply.github.com>
2025-04-20 20:18:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+Console.WriteLine($"Enter first digit: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"You typed: {number}");
+
+Console.WriteLine("Choose operation: +, -, *, /");
+string operation = Console.ReadLine();
+Console.WriteLine($"You chose: {operation}");
+
+Console.WriteLine("Enter Second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"You entered: {number2}");
+
+if (operation == "+"){
+    int result = number + number2;
+    Console.WriteLine($"{result}");   
+}
+else if (operation == "-"){
+    int result = number - number2;
+    Console.WriteLine($"{result}");
+}
+
+if (operation == "*"){
+    int result = number * number2;
+    Console.WriteLine($"{result}");
+}
+else if (operation == "/"){
+    decimal result = (decimal)number / (decimal)number2;
+    Console.WriteLine($"{result}");
+}
